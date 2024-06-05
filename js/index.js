4b50049197de00283483b84cea83a423d3176f41
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-06-05 21:41:23 +0500

    salom

---
+++ js/index.js
@@ -0,0 +1,485 @@
+// Boolean1
+// let a = +prompt("son kiriting");
+// let rezul = "";
+// if(a>0){
+//     rezul=("musbat");
+
+// } else {
+//     rezul=("manfiy");
+
+// }
+// console.log(rezul);
+
+
+
+// Boolean2
+// let a = +prompt("son kiriting");
+// let rezul = "";
+// if(a%2==1){
+//     rezul=("toq son");
+
+// } else {
+//     rezul=("juft son");
+
+// }
+// console.log(rezul);
+
+
+// Boolean3
+// let a = +prompt("son kiriting");
+// let rezul = "";
+// if(a%2==0){
+//     rezul=("juft son");
+
+// } else {
+//     rezul=("toq son");
+
+// }
+// console.log(rezul);
+
+// Boolean4
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = "";
+// if (a>2 && b<=3){
+// c=("to`g`ri");
+// } else {
+//     c=("xato");
+// }
+// console.log(c);
+
+// Boolean5
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = "";
+// if (a>=0 || b<-2){
+// c=("to`g`ri");
+// } else {
+//     c=("xato");
+// }
+// console.log(c);
+
+// Boolean6
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let y = "";
+// if (a<=b && b<=c){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+
+// Boolean7
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let y = "";
+// if (a<b && b<c || a>b && b>c){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+
+// Boolean8
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let y = "";
+// if (a%2==1 && b%2==1){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+
+// Boolean9
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let y = "";
+// if (a%2==1, b%2==0 || a%2==0, b%2==1){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+// Boolean10
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let y = "";
+// if (a%2==1, b%2==0 && a%2==0, b%2==1){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+// Boolean11
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let y = "";
+// if (a%2==1, b%2==1 || a%2==0, b%2==0){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+// Boolean12
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let y = "";
+// if (a>0 && b > 0 && c > 0){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+
+
+// Boolean13
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let y = "";
+// if (a>0 || b > 0 || c > 0){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+
+// Boolean14
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let y = "";
+// if (a>0, b < 0, c < 0 && b>0, a < 0, c < 0 && c>0, b < 0, a < 0){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+// Boolean15
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let y = "";
+// if (a>0, b > 0, c < 0 && b>0, a < 0, c > 0 && c>0, b < 0, a > 0){
+// y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+
+// Boolean16
+// let a = +prompt("birinchi sonni kiriting");
+// let y = "";
+// if (a/10>0 && a%2==0){
+//     y=("to`g`ri");
+// } else {
+//     y=("xato");
+// }
+// console.log(y);
+
+
+// if 1
+// let a = +prompt("bir sonni kiriting");
+// if (a>0){
+//     y= a+1;
+// } else {
+//     y=a;
+// }
+// console.log(y);
+
+
+// if 2
+// let a = +prompt("bir sonni kiriting");
+// if (a>0){
+//     y= a+1;
+// } else {
+//     y=a-2;
+// }
+// console.log(y);
+
+
+// if 3
+// let a = +prompt("bir sonni kiriting");
+// if (a>0){
+//     y= a+1;
+    
+// } else {
+  
+//     y=a-2;    
+// }
+// if (a==0){
+//     a=10;
+//     y=a;
+// }
+// console.log(y);
+
+// if 4
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let n =0;
+// if (a>0){
+//     n= n+1;
+// } else{
+
+// }
+// if (b>0){
+//     n= n+1;
+// } 
+// if (c>0){
+//     n= n+1;
+// } 
+
+// console.log(n);
+
+
+// if 5
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinch sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let n =0;
+// if (a>0){
+//     n= n+1;
+// } else{
+
+// }
+// if (b>0){
+//     n= n+1;
+// } 
+// if (c>0){
+//     n= n+1;
+// } 
+
+// console.log(n);
+
+// if 6
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let n =0;
+// if (a>b){
+//     n= a;
+// } else{
+//     n=b;
+// }
+
+// console.log(n);
+
+// // if 7
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let n = "";
+// if (a<b){
+    
+//     b = 1;
+//     n = b;
+
+// } else{
+    
+//     a= 2;
+//     n = a;
+// }
+
+// console.log(n);
+
+
+// if 8
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+
+// if (a<b){
+    
+//     console.log(b,a);
+
+
+// } else{
+// console.log(a,b);
+
+// }
+
+// if 9
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let n = 0;
+// if (a<b){
+    
+//     console.log(a,b);
+
+
+// } else{
+//     n=a-b;
+//     a=a-n;
+//     b=b+n;
+
+// console.log(a,b);
+
+// }
+
+
+// if 10
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let n = 0;
+// if (a!=b){
+    
+//     n=a+b;
+//     b=n+b;
+//     a=a+n;
+
+
+// } else{
+//   a=0;
+//   b=0;
+// }
+// console.log(a,b);
+
+
+// if 11
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let n = 0;
+// if (a!=b){
+//     if(a>b){
+//         n=a+a;
+//         b=b+a;
+//         a=n;
+//     } else{
+//         n=a+b;
+//         b=b+b;
+//         a=n;
+
+//     }
+ 
+// } else{
+//   a=0;
+//   b=0;
+// }
+// console.log(a,b);
+
+
+// if 12
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinch sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let n = "";
+// let y = "";
+
+// if (a<b){
+//    n=a;
+// } else{
+//  n=b;
+// }
+// if(n<c){
+//     y=n;
+// } else{
+//     y=c
+// }
+// console.log(y);
+
+
+// if 13
+// let a = +prompt("bir sonni kiriting");
+// let b = +prompt("ikkinch sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let n = "";
+// let y = "";
+
+// if (a<b){
+//    n=a;
+// } else{
+//  n=b;
+// }
+// if(n>c){
+//     y=n;
+// } else{
+//     y=c
+// }
+// console.log(y);
+
+
+
+// Boolean14
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let n = "";
+// let y = "";
+// let or = "";
+// if (a>b){
+// n=b;
+// } else 
+// {
+//   n=a;
+// } 
+// if (n<c){
+//     n=n;
+// } else{
+//     n=c;
+// }
+// if (a>b){
+//     y=a;
+//     } else 
+//     {
+//       y=b;
+//     } 
+//     if (y>c){
+//         y=y;
+//     } else{
+//         y=c;
+//     }
+// if (a<b){
+//    o=a;
+// } else{
+//  o=b;
+// }
+// if(o>c){
+//     or=o;
+// } else{
+//     or=c;
+// }
+// console.log(n,or,y)
+
+
+// Boolean15
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+// let y = "";
+// let or = "";
+
+// if (a>b){
+//     y=a;
+//     } else 
+//     {
+//       y=b;
+//     } 
+//     if (y>c){
+//         y=y;
+//     } else{
+//         y=c;
+//     }
+// if (a<b){
+//    o=a;
+// } else{
+//  o=b;
+// }
+// if(o>c){
+//     or=o;
+// } else{
+//     or=c;
+// }
+// console.log(or,y);
